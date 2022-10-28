63a7d368a5fc015c7e7611c24a89a76083a41e63
htomshi4 <115897131+htomshi4@users.noreply.github.com>
2022-10-28 17:36:10 +0300

    Update Employee.java

--- ManagerSystem/Employee.java
+++ ManagerSystem/Employee.java
@@ -9,49 +9,49 @@ package ManagerSystem;
  * @author mexawo3tebi
  */
 public class Employee extends Person {
-    
+
     @Override
+    public String toString() {
+        return """
+               Employee
+               Name:""" + super.name + "\naddress:" + super.address + "\njob_title:" + super.job_title + "\ndepartement:" + super.departement + "\nsalary:" + super.salary ;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
 
-    @Override
     public void setAddress(String address) {
         this.address = address;
     }
 
-    @Override
     public void setJob_title(String job_title) {
         this.job_title = job_title;
     }
 
-    @Override
     public void setDepartement(String departement) {
         this.departement = departement;
     }
 
-    @Override
     public String getName() {
         return name;
     }
 
-    @Override
     public String getAddress() {
         return address;
     }
-    @Override
     public String getJob_title() {
         return job_title;
     }
-    @Override
     public String getDepartement() {
         return departement;
     }
 
-    public Employee(String name, String address, String job_title, String departement, double salary) {
-        super(name, address, job_title, departement, salary);
+    public Employee(String name, String address, String job_title, String departement, double salary,int user_id) {
+        super(name, address, job_title, departement, salary,user_id);
     }
 
+    
     @Override
     public double getSalary() {
         return salary;
